fcd228c87e00ade69035ec950e8273c41b7640ac
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-09-09 10:45:31 -0400

    added contact info

--- app/views.py
+++ app/views.py
@@ -8,12 +8,12 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
-    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'name' : 'Geoffrey Sechter',
+    'bio' : 'Django NYC co-organizer, Django Girls Support team',
+    'email' : 'geoffrey.sechter@gmail.com', # Leave blank if you'd prefer not to share your email with other conference attendees
+    'twitter_username' : 'gsechter', # No @ symbol, just the handle.
+    'github_username' : "lightstrike", 
+    'headshot_url' : 'https://pbs.twimg.com/profile_images/593959225149140992/_oEj27YJ_400x400.jpg', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
 def home(request):
